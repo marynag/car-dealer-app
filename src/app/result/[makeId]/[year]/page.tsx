c02ee143816159b8208b018a0f502041f045c8b5
marynag <mgrabchak1109@gmail.com>
2024-11-27 16:38:59 +0200

    loader

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -1,5 +1,7 @@
 import { getVehicles } from '@/src/app/actions';
+import { Loading } from '@/src/components/loading/Loading';
 import { Result } from '@/src/components/title/Result';
+import { Suspense } from 'react';
 
 interface IPageProps {
   params: IParams;
@@ -18,7 +20,9 @@ const Page = async ({ params }: IPageProps) => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex w-full flex-col row-start-2 items-center ">
-        <Result data={data ?? []} success={success} />
+        <Suspense fallback={<Loading />}>
+          <Result data={data ?? []} success={success} />
+        </Suspense>
       </div>
     </div>
   );
